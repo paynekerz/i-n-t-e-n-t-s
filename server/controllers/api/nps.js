043cf1b95872970150e71a4a056c850ca4e8998a
Adaspeaks <adaspeaks@gmail.com>
2021-08-09 11:25:16 -0700

    nps get function

---
+++ server/controllers/api/nps.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const axios = require('axios').default;
+
+router.get('/', async (req, res) => {
+    try {
+        const stateCode = req.query.statecode;
+
+        const getParksByState = await axios.get(
+            `https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&api_key=${process.env.NPS_API_KEY}`
+        );
+        res.json(getParksByState.data.results);
+    } catch (err) {
+        res.status(404).json(err);
+    }
+});
